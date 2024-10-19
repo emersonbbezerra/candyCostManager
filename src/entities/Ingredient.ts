94553d211bb9c8cc484d49ccf7da99990ae1be17
Emerson Bezerra <emersonbbezerra@gmail.com>
2024-10-19 20:33:39 -0300

    feat: :sparkles: add Ingredient entity

---
+++ src/entities/Ingredient.ts
@@ -0,0 +1,22 @@
+import { uuidv7 } from "uuidv7";
+import { date } from "zod";
+
+export class Ingredient {
+  public readonly id!: string;
+
+  public name!: string;
+  public manufacturer!: string;
+  public price!: number;
+  public unitOfMeasure!: string;
+  public category!: string;
+  public createdAt?: string;
+  public updatedAt?: string;
+
+  constructor(props: Omit<Ingredient, "id">, id?: string) {
+    Object.assign(this, props);
+
+    if (!id) {
+      this.id = uuidv7();
+    }
+  }
+}
