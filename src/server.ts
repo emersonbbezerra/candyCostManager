910820dbe15ffd5810054412063ade92b8e2690b
Emerson Bezerra <emersonbbezerra@gmail.com>
2024-10-21 19:22:24 -0300

    feat: :card_file_box: add mongodb connection with mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import { app } from "./app";
 import dotenv from "dotenv";
+import { connect } from "./infra/database/mongoose";
 
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => {
+  connect();
+  console.log(`Server is running on port ${port}`);
+});
