742296ff6894104924491d24de66d31a44925356
Emerson Bezerra <emersonbbezerra@gmail.com>
2024-10-21 22:22:50 -0300

    refactor: :sparkles: add more validations to zod

--- src/useCases/CreateIngredient/CreateIngredientUseCase.ts
+++ src/useCases/CreateIngredient/CreateIngredientUseCase.ts
@@ -4,20 +4,32 @@ import { IIngredientsRepository } from "../../repositories/IIngredientsRepositor
 import { Ingredient } from "../../entities/Ingredient";
 import { z } from "zod";
 
+const capitalize = (str: string) => {
+  return str
+    .toLowerCase()
+    .replace(/(?:^|\s|[\p{P}\p{S}])\p{L}/gu, (char) => char.toUpperCase());
+};
+
 const ingredientSchema = z.object({
   name: z
     .string()
-    .min(3, { message: "O nome deve ter pelo menos 3 caracteres." }),
+    .min(3, { message: "O nome deve ter pelo menos 3 caracteres." })
+    .transform(capitalize),
   manufacturer: z
     .string()
-    .min(3, { message: "O fabricante deve ter pelo menos 3 caracteres." }),
+    .min(3, { message: "O fabricante deve ter pelo menos 3 caracteres." })
+    .transform(capitalize),
   price: z
     .number()
     .nonnegative({ message: "O preço deve ser um número não negativo." }),
   unitOfMeasure: z
     .string()
-    .min(1, { message: "A unidade de medida é obrigatória." }),
-  category: z.string().min(1, { message: "A categoria é obrigatória." }),
+    .min(1, { message: "A unidade de medida é obrigatória." })
+    .transform(capitalize),
+  category: z
+    .string()
+    .min(1, { message: "A categoria é obrigatória." })
+    .transform(capitalize),
   createdAt: z.date().optional().nullable(),
   updatedAt: z.date().optional().nullable(),
 });
