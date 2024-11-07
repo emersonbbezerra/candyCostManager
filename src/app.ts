4af0495d3a8a3ea8e79b40a0b83d79520ef724b9
Emerson Bezerra <emersonbbezerra@gmail.com>
2024-11-06 22:24:36 -0300

    feat: :sparkles: add CORS support and updated dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 import express from "express";
+import cors from "cors";
 import { componentsRoutes } from "./routes/componentRoutes";
 import { productsRouter } from "./routes/productRoutes";
 import { userRoutes } from "./routes/userRoutes";
 import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware";
 
 const app = express();
+const corsOptions = {
+  origin: process.env.URL_API,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  optionsSuccessStatus: 200,
+};
 
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(userRoutes);
 app.use(componentsRoutes);
